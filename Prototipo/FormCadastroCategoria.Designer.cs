2e67215464f7490b7a9d462de61223b17cdf1cbd
Felipe Fischer <felipecorreiafischer@hotmail.com>
2017-04-18 07:29:37 -0300

    Deletado Classe SqlAcessoAoBanco, ajustado classe categoria.

--- Prototipo/FormCadastroCategoria.Designer.cs
+++ Prototipo/FormCadastroCategoria.Designer.cs
@@ -102,7 +102,6 @@
             this.Controls.Add(this.txtBoxNome);
             this.Controls.Add(this.txtBoxId);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCadastroCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Categoria";
