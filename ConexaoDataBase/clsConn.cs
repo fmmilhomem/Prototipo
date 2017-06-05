d98fe060b80e6c92cb889b3e01646a8678b6d0e7
Felipe Milhomem <fmmilhomem@gmail.com>
2017-06-05 19:10:56 -0300

    descomentando conexao local

--- ConexaoDataBase/clsConn.cs
+++ ConexaoDataBase/clsConn.cs
@@ -10,7 +10,7 @@ namespace ConexaoDataBase
     class clsConn
     {
         //CONEXÃO LOCAL
-        public static SqlConnection Conectar()
+       /* public static SqlConnection Conectar()
         {
             SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
             sb.UserID = "sa";
@@ -21,8 +21,8 @@ namespace ConexaoDataBase
             SqlConnection cn = new SqlConnection(sb.ConnectionString);
             cn.Open();
             return cn;
-        }
-        /*
+        }*/
+        
         //CONEXÃO AZURE
         public static SqlConnection Conectar()
         {
@@ -42,7 +42,7 @@ namespace ConexaoDataBase
                 //
             }
             return cn;
-        }*/
+        }
          
     }
 }
